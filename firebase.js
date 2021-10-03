a705427a27cc1c67441ba2ce0b3de60a7ec0792c
Devang Saxena <devangsaxena2018@gmail.com>
2021-10-03 21:45:30 +0530

    cart structure

---
+++ firebase.js
@@ -0,0 +1,9 @@
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyCQOYkHVlkCn4jGXahZQvcYKfmDP-n8u5M",
+  authDomain: "codis-f6a7b.firebaseapp.com",
+  projectId: "codis-f6a7b",
+  storageBucket: "codis-f6a7b.appspot.com",
+  messagingSenderId: "240892312016",
+  appId: "1:240892312016:web:1fcdc51b5d81b7c07861d5"
+};
